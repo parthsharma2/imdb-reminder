c1509da9558369ec792b0f5c710d2d191fa1cf3e
Parth Sharma <parth261297@gmail.com>
2018-10-07 18:06:05 +0530

    Add command line argument support

--- imdb_reminder.py
+++ imdb_reminder.py
@@ -1,19 +1,17 @@
 import argparse
 import requests
+import logging
 from bs4 import BeautifulSoup
 from lxml import html
 from datetime import datetime
 
 
-# parser = argparse.ArgumentParser(description='Send email reminders for TV-Shows.')
-#
-# parser.add_argument('email', metavar='email',
-#                     help='Email to send the reminders to.')
-# parser.add_argument('shows', nargs='+', help='Comma seperated Shows.')
-#
-# args = parser.parse_args()
-# email = args.email
-# shows = ' '.join(args.shows).split(','))
+parser = argparse.ArgumentParser(description='Send email reminders for TV-Shows.')
+
+parser.add_argument('email', metavar='EMAIL',
+                    help='Email to send the reminders to.')
+parser.add_argument('shows', metavar='SHOWS',
+                    nargs='+', help='Comma seperated Shows.')
 
 
 def get_show_url(name):
@@ -39,7 +37,7 @@ def get_show_status(url):
     r = requests.get(latest_season_url)
     doc = html.fromstring(r.content)
 
-    episodes = doc.xpath('//*[@id="episodes_content"]/div[2]/div[2]/div')
+    episodes = doc.xpath('//*[@id="episodes_content"]/div/div[2]/div')
 
     # Check whether the first episode is yet to be aired
     first_epi = episodes[0]
@@ -47,9 +45,35 @@ def get_show_status(url):
         airdate = first_epi.xpath('*//div[@class="airdate"]/text()')[0].strip()
         return 'The next season begins in {}'.format(airdate)
 
-    # Check other episodes if aired
+    # Check other episodes yet to be aired
     for episode in episodes[1:]:
         if episode.xpath('*//span[contains(@class,"add-image-container")]'):
             date = episode.xpath('*//div[@class="airdate"]/text()')[0].strip()
             airdate = datetime.strptime(date, '%d %b. %Y')
             return 'Next episode airs on {}'.format(airdate.strftime('%Y-%m-%d'))
+
+
+def main():
+    args = parser.parse_args()
+    email = args.email
+    shows = ' '.join(args.shows).split(',')
+    statuses = []
+    logging.info('Email: ' + email)
+    logging.info('Shows: ' + str(shows))
+    for show in shows:
+        logging.info('Fetching: ' + show)
+        url = get_show_url(show)
+        status = get_show_status(url)
+        statuses.append(status)
+
+    logging.info('Generating message')
+    msg = ""
+    for show, status in zip(shows, statuses):
+        msg += 'TV Series Name: ' + show.title() + '\n'
+        msg += 'Status: ' + status + '\n\n'
+
+    print(msg.strip())
+
+
+if __name__ == '__main__':
+    main()
