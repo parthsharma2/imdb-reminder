91f858a95f7f7de3fb94a844b67836b1922225ea
Parth Sharma <parth261297@gmail.com>
2018-10-10 18:39:25 +0530

    Improve documentation

--- imdb_reminder.py
+++ imdb_reminder.py
@@ -1,11 +1,11 @@
 import argparse
-import requests
-import logging
 import configparser
+import logging
+import requests
 import smtplib
 import sys
-from lxml import html
 from datetime import datetime
+from lxml import html
 
 
 config = configparser.ConfigParser()
@@ -23,7 +23,7 @@ except KeyError as e:
 logging.basicConfig(filename='imdb_reminder.log', level=logging.DEBUG)
 
 # Set argparse config
-parser = argparse.ArgumentParser(description='Send email reminders for TV-Shows.')
+parser = argparse.ArgumentParser(description='Send email reminders for TV-Shows')
 parser.add_argument('email', metavar='EMAIL',
                     help='Email to send the reminders to.')
 parser.add_argument('shows', metavar='SHOWS',
@@ -32,35 +32,56 @@ args = parser.parse_args()
 
 
 def get_show_url(name):
-    """Get the url of the show."""
+    """Get the url of the show.
+
+    Args:
+        name (str): The name of the show.
+
+    Returns:
+        str: The imdb url of the show.
+
+    """
     url = 'https://imdb.com/find?q={}'
     r = requests.get(url.format(name.replace(' ', '+')))
     doc = html.fromstring(r.content)
 
-    # Extract link from the first search result
+    # Extract url from the first search result
     rel = doc.xpath('//td[@class="result_text"]/a/@href')[0]
 
     return 'https://www.imdb.com{}'.format(rel)
 
 
 def get_show_status(url):
-    """Get the status of the show."""
+    """Get the status of the show.
+
+    Args:
+        url (str): imdb url of a show.
+
+    Returns:
+        str: Status of the show.
+
+    """
     r = requests.get(url)
     doc = html.fromstring(r.content)
 
+    # Extract runtime from the show page.
+    # If runtime is not a single year the show has finished streaming.
     runtime = doc.xpath('//div[@class="subtext"]/a/text()')[-1]
     if runtime[runtime.index('–') + 1:-2].isdigit():
         return 'The show has finished streaming all its episodes.'
 
+    # Get the latest season of the show.
     rel = doc.xpath('//div[@class="seasons-and-year-nav"]/div[3]/a[1]/@href')[0]
     latest_season_url = 'https://www.imdb.com{}'.format(rel)
 
     r = requests.get(latest_season_url)
     doc = html.fromstring(r.content)
 
+    # Extract episode listing of the latest season.
     episodes = doc.xpath('//*[@id="episodes_content"]/div/div[2]/div')
 
-    # Check whether the first episode is yet to be aired
+    # Check whether the first episode is yet to be aired.
+    # If true return the season air year.
     first_epi = episodes[0]
     if first_epi.xpath('*//span[contains(@class,"add-image-container")]'):
         airdate = first_epi.xpath('*//div[@class="airdate"]/text()')[0].strip()
@@ -75,6 +96,12 @@ def get_show_status(url):
 
 
 def send_email(msg):
+    """Send email.
+
+    Args:
+        msg (str): The message to sent in the email.
+
+    """
     smtp = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
     smtp.starttls()
     smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
@@ -85,14 +112,15 @@ def send_email(msg):
     smtp.sendmail(SENDER_EMAIL, args.email, mail_msg)
 
 
-
 def main():
     """Driver function of the script."""
     email = args.email
     shows = ' '.join(args.shows).split(',')
     statuses = []
+
     logging.info('Email: %s', email)
     logging.info('Shows: %s', str(shows))
+
     print('Fetching show details...')
     for show in shows:
         logging.info('Fetching: %s', show)
@@ -110,11 +138,6 @@ def main():
     logging.info('Sending mail')
     send_email(msg)
     print('Email sent!')
-    # try:
-    #     send_email(msg)
-    #     print('Email sent!')
-    # except SMTPException as e:
-    #     logging.error()
 
 
 if __name__ == '__main__':
